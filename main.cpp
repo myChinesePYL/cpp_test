dbe58b422e540dbdbbde5bece563520ec764efcc
myChinesePYL <3223285598@qq.com>
2024-02-18 16:12:51 +0800

    修复了文件找不到的错误

--- main.cpp
+++ main.cpp
@@ -5,6 +5,11 @@
 int main() {
   InitWindow(500, 500, "raylib test");
   SetTargetFPS(60);
+  // 加载必要文件
+  LoadTexture("./imgs/heart_blue.png");
+  LoadTexture("./imgs/heart.png");
+  LoadFont("./fonts/ut.ttf");
+  LoadFont("./fonts/sans.ttf");
 
   while (!WindowShouldClose()) {
     BeginDrawing();
