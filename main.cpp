94d6aecce5af9b7d206df5ddf199322e335560e0
myChinesePYL <3223285598@qq.com>
2024-02-20 09:53:29 +0800

    testing

--- main.cpp
+++ main.cpp
@@ -1,22 +1,28 @@
 #include "raylib.h"
 #include "raymath.h"
 #include "./startMenu.cpp"
+#include <transition/transition.h>
 
 int main() {
-  InitWindow(500, 500, "raylib test");
+  InitWindow(500, 500, "NNSans");
   SetTargetFPS(60);
-  // 加载必要文件
-  LoadTexture("./imgs/heart_blue.png");
-  LoadTexture("./imgs/heart.png");
-  LoadFont("./fonts/ut.ttf");
-  LoadFont("./fonts/sans.ttf");
+  Texture2D texture_heart_blue = LoadTexture("./imgs/heart.png");
+  Texture2D texture_heart = LoadTexture("./imgs/heart.png");
+  Font font_ut = LoadFont("./fonts/ut.ttf");
+  Font font_sans = LoadFont("./fonts/sans.ttf");
+  Font font_selector = LoadFont("./fonts/selector.ttf");
+  Font font_miss = LoadFont("./fonts/miss.ttf");
+  Sound audio_bgm = LoadSound("./audio/Xomu_Last_Dance.ogg");
+  SetExitKey(KEY_Q);
 
   while (!WindowShouldClose()) {
+    while (IsWindowReady()) break;
     BeginDrawing();
 
     ClearBackground(BLACK);
-    startMenu();
+    startMenu.startMenu(font_selector, font_selector, texture_heart);
 
+    DrawFPS(10,10);
     EndDrawing();
   }
   CloseWindow();
--- startMenu.cpp
+++ startMenu.cpp
@@ -1,7 +1,18 @@
 #include "raylib.h"
+#include "raymath.h"
+#include <transition/transition.h>
+#include <iostream>
 
-void startMenu() {
-  // 实现开始菜单的代码
-  // 包括游戏标题、开始按钮等
-  
+class __StartMenu {
+  private:
+    Transition title_transition = Transition(0,100);
+  public:
+    void startMenu(Font font_title, Font font_selector, Texture2D texture_heart) {
+      // 实现开始菜单的代码
+      // 包括游戏标题、开始按钮等
+      auto a = [&](double x){};
+      title_transition.onUpdate = [&](double x){};
+      // {DrawTextEx(font_title, "NNSans", Vector2{(float)x*140, 100}, 80, 2, (Color){255,255,255,255});};
+    };
 };
+__StartMenu startMenu = __StartMenu();
