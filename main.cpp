1853388d4ca87e41283a3f3bb8630632dc4032b9
myChinesePYL <3223285598@qq.com>
2024-03-16 11:38:50 +0800

    添加了FTXUI库

--- main.cpp
+++ main.cpp
@@ -1,31 +1,9 @@
-#include "raylib.h"
-#include "raymath.h"
-#include "./startMenu.cpp"
-#include <transition/transition.h>
+#include <iostream>
 
 int main() {
-  InitWindow(500, 500, "NNSans");
-  SetTargetFPS(60);
-  Texture2D texture_heart_blue = LoadTexture("./imgs/heart.png");
-  Texture2D texture_heart = LoadTexture("./imgs/heart.png");
-  Font font_ut = LoadFont("./fonts/ut.ttf");
-  Font font_sans = LoadFont("./fonts/sans.ttf");
-  Font font_selector = LoadFont("./fonts/selector.ttf");
-  Font font_miss = LoadFont("./fonts/miss.ttf");
-  Sound audio_bgm = LoadSound("./audio/Xomu_Last_Dance.ogg");
-  SetExitKey(KEY_Q);
-
-  while (!WindowShouldClose()) {
-    while (IsWindowReady()) break;
-    BeginDrawing();
-
-    ClearBackground(BLACK);
-    startMenu.startMenu(font_selector, font_selector, texture_heart);
-
-    DrawFPS(10,10);
-    EndDrawing();
+  for(int i = 0; i < 10; i++){
+    std::cout << std::string(i, 'm') << std::endl;
   }
-  CloseWindow();
 
   return 0;
 }
