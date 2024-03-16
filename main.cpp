26de449aa7bfd6fe308228fe23ce3c0a33c221ca
myChinesePYL <3223285598@qq.com>
2024-03-16 12:16:23 +0800

    添加README.md

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 // #include <iostream>
-#include <ftxui/screen/screen.hpp>
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/component/component.hpp>
 
 int main() {
+  // init
   auto screen = ftxui::ScreenInteractive::TerminalOutput();
+  auto component = ftxui::Renderer([&] {
+    return ftxui::vbox({
+      ftxui::text("Hello"),
+      ftxui::text("World"),
+    });
+  });
 
   return 0;
 }
