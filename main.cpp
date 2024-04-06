323797a1f68aa9ca79d8f0a4ed09b1711ea45ee9
myChinesePYL <3223285598@qq.com>
2024-04-06 13:05:30 +0800

    ...

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main(int argc, char *argv[]) {
+  std::printf("Running App\n");
+  std::printf("Checking File...\n");
+  std::printf("\tuserkey.json...");
+  std::printf("ok\n");
+
+  return 0;
+}
