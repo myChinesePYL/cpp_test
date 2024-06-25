d829584be8fae3c76abf3d71ff2361de508060d1
mychinesepyl <3223285598@qq.com>
2024-06-25 16:36:44 +0800

    给Color类加了个透明的值, 所以3维向量变成4维向量

--- src/core/Color.hpp
+++ src/core/Color.hpp
@@ -3,9 +3,12 @@
 
 class Color {
 	public:
-		vec3 RGB;
+		vec4 RGBA;
 
 		Color(vec3 rgb) {
-			RGB.set(rgb);
+			RGBA.set(rgb.X, rgb.Y, rgb.Z, 1.0f);
+		}
+		Color(vec4 rgba) {
+			RGBA.set(rgba);
 		}
 };
